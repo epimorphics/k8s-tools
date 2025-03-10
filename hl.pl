4ced991e7968c18b99f4d56b5a90ce28e0e76fdf
andrew-pickin-epi <Andrew.Pickin@epimorphics.com>
2025-03-10 11:44:24 +0000

    list-nodes has az

--- hl.pl
+++ hl.pl
@@ -6,13 +6,21 @@ while(<>){
   $ready = ($#field > 4) ? 2 : 1; # if namespace displayed Read is #2 field else #1
   $phase = 1+$ready;
 
-  print "$_";
-  if ($field[$phase] eq "Failed") {
-    print "\t\t*!*"
-  }
-  elsif ($field[$phase] ne "Terminating" && $field[$phase] ne "Completed") {
+  if ($field[$phase] ne "Terminating") {
+    $a = $b = " ";
+    $l = "|";
+    $a = $b = $l = "*" if ($field[$phase] eq "Failed" || $field[$phase] eq "Error");
+    $a = $b = "~" if ($field[$phase] eq "PodInitializing");
+    $a = $b = "." if ($field[$phase] eq "Pending");
+    $a = $b = "-" if ($field[$phase] eq "ContainerCreating");
+    $a = "=" if ($field[$phase] eq "Running");
+    $b = ">" if ($field[$phase] eq "Running");
+    $a = $b = "/" if ($field[$phase] eq "Completed");
     @r = split('/', $field[$ready]);
-    print "\t\t|", "+" x $r[0], "-" x ($r[1]-$r[0]), "|";
+    $s = sprintf("%s%s%s%s", $l, $a x $r[0], $b x ($r[1]-$r[0]), $l);
+    printf("%-12s %s\n", $s, $_);
   }
-  print "\n";
+  else {
+    printf("%-12s %s\n", "", $_);
+  }
 }
